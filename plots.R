63928b93d9110e038c06319a08d9e1d49156f56f
Helena Rasche <hxr@hx42.org>
2022-11-07 11:23:54 +0100

    Add fr

--- plots.R
+++ plots.R
@@ -1,15 +1,19 @@
 library(tidyverse)
-numbers_eu <- read_csv("https://usegalaxy.eu/tiaas/numbers.csv")
+#numbers_eu <- read_csv("https://usegalaxy.eu/tiaas/numbers.csv")
+numbers_eu <- read_csv("numbers.eu.csv")
 numbers_eu$site = 'eu'
-numbers_au <- read_csv("https://usegalaxy.org.au/tiaas/numbers.csv")
+numbers_fr <- read_csv("https://usegalaxy.fr/tiaas/numbers.csv")
+numbers_fr$site = 'fr'
+#numbers_au <- read_csv("https://usegalaxy.org.au/tiaas/numbers.csv")
+numbers_au <- read_csv("numbers.org.au.csv")
 numbers_au$site = 'au'
-numbers <- do.call(rbind, list(numbers_au, numbers_eu))
+numbers <- do.call(rbind, list(numbers_au, numbers_eu, numbers_fr))
 
-ggplot(numbers, aes(start, fill=site)) + 
+ggplot(numbers, aes(start, fill=site)) +
   geom_histogram(position="stack") +
-  xlab("Start Date") + ylab("Number of Events") + 
+  xlab("Start Date") + ylab("Number of Events") +
   ggtitle("Events over time") +
-  scale_fill_manual(values=c("#D52D00", "#FF9A56"))
+  scale_fill_manual(values=c("#D52D00", "#EF7627", "#FF9A56"))
 ggsave("event-starts.png", width=5, height=2)
 system(paste("optipng ", getwd(), "event-starts.png"))
 
@@ -17,6 +21,6 @@ ggplot(numbers, aes(end-start, fill=site)) +
   geom_histogram(position="stack") +
   xlab("Event Length") + ylab("Number of Events") + 
   ggtitle("Event Lengths") +
-  scale_fill_manual(values=c("#D162A4", "#A30262"))
+  scale_fill_manual(values=c("#D162A4", "#B55690", "#A30262"))
 ggsave("event-lengths.png", width=5, height=2)
 system(paste("optipng ", getwd(), "event-lengths.png"))
