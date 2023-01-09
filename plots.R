5e2b429628c7f5c27f4a0d2e8f0c76ae4715e5b6
Helena Rasche <hexylena@galaxians.org>
2023-01-09 14:02:49 +0100

    move images

--- plots.R
+++ plots.R
@@ -17,13 +17,13 @@ ggplot(numbers, aes(start, fill=site)) +
   xlab("Start Date") + ylab("Number of Events") +
   ggtitle("Events over time") +
   scale_fill_manual(values=c("#D52D00", "#EF7627", "#FF9A56", "#FFCC56"))
-ggsave("event-starts.png", width=5, height=2)
-system(paste("optipng ", getwd(), "event-starts.png"))
+ggsave("images/event-starts.png", width=5, height=2)
+system(paste("optipng ", getwd(), "images/event-starts.png"))
 
 ggplot(numbers, aes(end-start, fill=site)) + 
   geom_histogram(position="stack") +
   xlab("Event Length (Days)") + ylab("Number of Events") + 
   ggtitle("Event Lengths") +
   scale_fill_manual(values=c("#F9A9D9", "#ED71BB", "#B55690", "#A30262"))
-ggsave("event-lengths.png", width=5, height=2)
-system(paste("optipng ", getwd(), "event-lengths.png"))
+ggsave("images/event-lengths.png", width=5, height=2)
+system(paste("optipng ", getwd(), "images/event-lengths.png"))
